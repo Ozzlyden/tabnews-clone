555e62640c4d2075afc136162455a81171a6ee5f
Victor Valente <Valente-victor@hotmail.com>
2023-09-04 15:51:18 -0300

    Desenvolvendo tests para uma Calculadora usando `jest`

---
+++ models/calculadora.js
@@ -0,0 +1,21 @@
+
+function somar(num1, num2) {
+  return num1 + num2;
+}
+
+function subitracao(num1, num2) {
+  return num1 - num2;
+}
+
+function divisao(num1, num2) {
+  return num1 / num2;
+}
+
+function multiplicacao(num1, num2) {
+  return num1 * num2;
+}
+
+exports.somar = somar;
+exports.subitracao = subitracao;
+exports.divisao = divisao;
+exports.multiplicacao = multiplicacao;
