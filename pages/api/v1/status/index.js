516ccd53fee23c8c56f587c3794a3f377553409a
Victor Valente <Valente-victor@hotmail.com>
2023-10-28 23:19:35 -0300

    Conexao usando pg no `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response){
+import database from '../../../../infra/database.js';
+
+async function status(request, response){
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.row);
+
   // response.status(200).send("Alunos do curso.dev são mais inteligentes"); // Enviar a resposta para API
   response.status(200).json({chave: "são a cima da media"}); // Tbm envia resposta para API usando charset=utf-8
 }
