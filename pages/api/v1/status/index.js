05054dc45c21b0b56e5e355eaa3f7ec77ee6f865
Victor Valente <Valente-victor@hotmail.com>
2023-12-06 16:24:18 -0300

    Teste de identificacao de data usando ISO 8601

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,11 @@
 import database from 'infra/database.js';
 
 async function status(request, response){
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.row);
+  const updatedAt = new Date().toISOString(); // Pega da data atual no formato ISO 8601
 
-  // response.status(200).send("Alunos do curso.dev são mais inteligentes"); // Enviar a resposta para API
-  response.status(200).json({chave: "são a cima da media"}); // Tbm envia resposta para API usando charset=utf-8
+  response.status(200).json({
+    updated_at: updatedAt,
+  }); // Tbm envia resposta para API usando charset=utf-8
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,10 @@ test("GET to /api/v1/status should return 200", async() => {
 
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();   // Conversao para json
+  expect(responseBody.updated_at).toBeDefined();  // Verificacao da existencia da updated_at
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Verificando o conteudo
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 });
