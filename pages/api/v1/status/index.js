acf9568b4fc75aaf2cc62242cbb70d93db1f4e99
Victor Valente <Valente-victor@hotmail.com>
2023-12-18 23:02:43 -0300

    Teste do numero maximo de conexoes do BD

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,11 +7,15 @@ async function status(request, response){
   const databaseVersionResult = await database.query("SHOW server_version;");   // var de consulta ao BD do Postgres
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;   // Resposta que a gente queria da consulta
 
+  const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies:{
       database:{
-        version: databaseVersionValue
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue)
       }
     }
   }); // Tbm envia resposta para API usando charset=utf-8
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,13 +1,15 @@
 test("GET to /api/v1/status should return 200", async() => {
 
+  // VARIVAEIS DE TESTE
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-
   const responseBody = await response.json();   // Conversao para json
-  expect(responseBody.updated_at).toBeDefined();  // Verificacao da existencia da updated_at
-
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Verificando o conteudo
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
+
+
+  // TESTES ESPERADOS
+  expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
