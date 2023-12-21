1a285d70077e3abd8d99b50330c4db1b3e7ea081
Victor Valente <Valente-victor@hotmail.com>
2023-12-21 20:18:57 -0300

    Finaliza `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -10,12 +10,24 @@ async function status(request, response){
   const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
   const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
 
+  // COMO EVITAR ATAQUE SQL INJECTION
+  const databaseName = process.env.POSTGREST_DB;
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+  const databaseOpenedConnectionValue = databaseOpenedConnectionResult.rows[0].count;
+
+  console.log(databaseOpenedConnectionValue);
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies:{
       database:{
         version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionValue)
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionValue
       }
     }
   }); // Tbm envia resposta para API usando charset=utf-8
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,11 +5,10 @@ test("GET to /api/v1/status should return 200", async() => {
   const responseBody = await response.json();   // Conversao para json
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Verificando o conteudo
 
-
-
   // TESTES ESPERADOS
   expect(response.status).toBe(200);
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
