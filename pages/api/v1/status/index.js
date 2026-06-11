1b645ff3e7b607faaa94009ef6eb24e81da05b83
Ozzlyden <valente-victor@hotmail.com>
2026-06-11 15:34:52 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,30 +1,31 @@
 import database from 'infra/database.js';
 
-async function status(request, response){
+async function status(request, response) {
+
+  // VARIAVEIS DE CONSULTA DO BD
   const updatedAt = new Date().toISOString(); // Pega da data atual no formato ISO 8601
 
-// VARIAVEIS DE CONSULTA DO BD
   const databaseVersionResult = await database.query("SHOW server_version;");   // var de consulta ao BD do Postgres
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;   // Resposta que a gente queria da consulta
 
-  const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
-  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
+  const databaseMaxConnectionResult = await database.query("SHOW max_connections;"); // Mostra no número maximo de conexões do BD
+  const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections; // Por padrão geralmente são 100
 
-  // COMO EVITAR ATAQUE SQL INJECTION
-  const databaseName = process.env.POSTGREST_DB;
+  // EX DE COMO EVITAR ATAQUE SQL INJECTION
+  const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionResult = await database.query({
+    // Para evitar, basta separar o texto dos valores que vc que injetar na Query
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName]
   });
 
   const databaseOpenedConnectionValue = databaseOpenedConnectionResult.rows[0].count;
 
-  console.log(databaseOpenedConnectionValue);
-
+  // Respostas da requisições HTTP com status OK (200)
   response.status(200).json({
     updated_at: updatedAt,
-    dependencies:{
-      database:{
+    dependencies: {
+      database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionValue),
         opened_connections: databaseOpenedConnectionValue
